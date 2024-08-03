20343ee7831c2d7eb18bb0a05764ad6e52ef707e
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-08-04 02:42:25 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double num3 = (double)num1;
+        double num4 = (double)num2;
+        return (int) ((num3 / num4) * 1000);
+    }
+}
