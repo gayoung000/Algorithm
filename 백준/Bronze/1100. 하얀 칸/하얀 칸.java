cdd1bc441d1e088ae991f96212729b7dc70a2b97
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-09-15 13:41:45 +0900

    [Bronze II] Title: 하얀 칸, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.java
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -1,51 +1,32 @@
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
 
-	static char[][] chessboard = new char[8][8];
-	static int[][] colorboard = new int[8][8]; 
-	static boolean isWhite = true;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for(int i = 0; i < 8; i++) {
-			String[] arr = br.readLine().trim().split(""); 
-			for(int j = 0; j < 8; j++) {
-				chessboard[i][j] = arr[j].charAt(0); 
-			}
-		}
-		 
-		for(int i = 0; i < 8; i+=2) {
-			for (int j = 0; j < 8; j++) {
-				if(isWhite) colorboard[i][j] = 1;
-				else colorboard[i][j] = 0;			
-				isWhite = !isWhite;
-			}
-		}
-		
-		isWhite = false;
-		
-		for(int i = 1; i < 8; i+=2) {
-			for (int j = 0; j < 8; j++) {
-				if(isWhite) colorboard[i][j] = 1;
-				else colorboard[i][j] = 0;	
-				isWhite = !isWhite;
-			}
-		}
-		
-		int count = 0;
-		
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
-				if(colorboard[i][j] == 1) {
-					if(chessboard[i][j] == 'F') count++;
-				}
-			}
-		}
-		
-		System.out.println(count);
-	
-	} //main
+    static char[][] chessboard = new char[8][8];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for (int i = 0; i < 8; i++) {
+            String[] arr = br.readLine().trim().split(""); 
+            for (int j = 0; j < 8; j++) {
+                chessboard[i][j] = arr[j].charAt(0);
+            }
+        }
+        
+        int count = 0;
+
+        for(int i = 0; i < 8; i++) {
+        	boolean isWhite = (i%2 == 0);
+        	for(int j = 0; j < 8; j++) {
+        		if(isWhite && chessboard[i][j] == 'F') count++;
+        		isWhite = !isWhite;
+        	}
+        }
+
+        System.out.println(count);
+    }
 }
