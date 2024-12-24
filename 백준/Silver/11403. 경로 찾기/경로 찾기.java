c8c75f75563020c4bda87515a7f3798461433c00
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-12-24 17:55:59 +0900

    [Silver I] Title: 경로 찾기, Time: 492 ms, Memory: 31112 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[][] graph = new int[n][n];
+        int[][] result = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = sc.nextInt();
+                result[i][j] = graph[i][j];
+            }
+        }
+        
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (result[i][k] == 1 && result[k][j] == 1) {
+                        result[i][j] = 1;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+}
