37bd40725f33fb4ea4ccc4f1805c2046c83ae86b
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2025-07-15 23:29:11 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,20 +1,4 @@
 function solution(numbers, num1, num2) {
-    // let answer = [];
-    // answer = [...numbers];
-    // answer = answer.splice(num1, num2);
-    // return answer;
-    
-    // let answer = [];
-    // if(num1 >= 0 && num2 > num1){
-    //     answer = numbers.splice(num1, num2);
-    // } else if (num1 == num2){
-    //     answer = numbers[0];
-    // }
-    // return answer;
-    
-    let answer = [];
-    answer = [...numbers];
-    let result = answer.slice(num1, num2+1);
-    return result
+    return numbers.slice(num1, num2+1);
 
 }
