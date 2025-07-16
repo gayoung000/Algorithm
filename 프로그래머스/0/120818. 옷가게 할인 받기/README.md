3ed689ab9d06551efe97a3bb12d6f876b7d965b9
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2025-07-16 19:00:06 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/README.md
+++ 프로그래머스/0/120818. 옷가게 할인 받기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옷가게 할인 받기 - 120818 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818?language=javascript) 
 
 ### 성능 요약
 
-메모리: 68 MB, 시간: 0.03 ms
+메모리: 33 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 23:45:31
+2025년 07월 16일 19:00:04
 
 ### 문제 설명
 
--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,13 +1,11 @@
 function solution(price) {
-     
-    if ( price >= 100000 && price < 300000){
-        price = price - (price * 0.05);
-    } else if ( price >= 300000 && price < 500000){
-        price = price - (price * 0.1);
-    } else if (price >= 500000){
-        price = price - (price * 0.2);
-    }
-    
-    price = parseInt(price);
-    return price;
+    price = price >= 500000
+        ? price * 0.8
+        : price >= 300000
+        ? price * 0.9
+        : price >= 100000
+        ? price * 0.95
+        : price;
+
+    return parseInt(price);
 }
