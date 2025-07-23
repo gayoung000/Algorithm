8646bed5e83d71bf8ba108144f602b6e1dc810b3
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2025-07-23 16:37:15 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,7 +1,3 @@
 function solution(my_string) {
-    const arr = my_string.split('');
-    const result = arr.map((str) => parseInt(str));
-    const filter2 = result.filter(a => typeof(a) == "number")
-    console.log(filter2);
     return my_string.split('').map((str) => parseInt(str)).filter(a => !Number.isNaN(a)).sort();
 }
