6d26de28fc05759a293e774d660735ebb92f986e
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2025-07-23 23:05:18 +0900

    [level 0] Title: 369게임, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,7 +1,6 @@
 function solution(order) {
     const result = order.toString().match(/[369]/g);
     let answer = 0;
-    result == null ? answer = 0 : answer = result.length;
+    return result == null ? answer = 0 : answer = result.length;
     
-    return answer
 }
