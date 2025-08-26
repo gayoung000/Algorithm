e6b8b24126fd2a37fcd3a9fe1a51746a8838f321
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2025-08-27 00:30:00 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,23 +1,14 @@
 function solution(my_string) {
-    let a = 0; // 플러스 담기
-    let b = 0; // 마이너스 담기 
-    let my_string2 = my_string.split(" ");
+    let arr = my_string.split(" ");
+    let result = +arr[0];
     
-    for(let i = 0; i < my_string2.length; i++){
-        if(i === 0){
-            a += parseInt(my_string2[i]);
-            continue
-        }
-        
-        if(parseInt(my_string2[i])){ // 만일 숫자라면
-            if((my_string2[i-1]) === "-"){
-                b += parseInt(my_string2[i]);
-            } else if((my_string2[i-1]) === "+") {
-                a += parseInt(my_string2[i]);
-            }
-        }else{
-            continue // 기호라면 넘어가겠다.
+    for(let i = 1; i < arr.length; i += 2){
+        if(arr[i] === "+"){
+            result += +arr[i+1];
+        } else if(arr[i] === "-"){
+            result += -arr[i+1];
         }
     }
-       return a-b;
+        
+    return result;
 }
