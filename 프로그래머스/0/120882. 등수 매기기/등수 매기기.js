b50079c2bb67197a35b59f57f3d8dc939731ccd4
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2025-09-16 23:48:53 +0900

    [level 0] Title: 등수 매기기, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,4 +1,24 @@
 function solution(score) {
-    var answer = [];
-    return answer;
+     let result = [];
+
+    for (let i = 0; i < score.length; i++) {
+        let [eng, math] = score[i];
+        let avg = (eng + math) / 2;
+
+        // 본인보다 평균이 높은 사람의 수를 센다
+        let rank = 1;
+        for (let j = 0; j < score.length; j++) {
+            if (i !== j) {
+                let [e, m] = score[j];
+                let otherAvg = (e + m) / 2;
+                if (otherAvg > avg) {
+                    rank++;
+                }
+            }
+        }
+
+        result.push(rank);
+    }
+
+    return result;
 }
