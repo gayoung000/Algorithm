c8ef7c255a3e6c9e7a00223948ef7dcac91cd95d
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2025-10-09 23:14:50 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -2,25 +2,28 @@ function solution(n, computers) {
     // 우선 연결 여부를 따지는 것일까 군집 개수를 따지는 것일까
     // 그래프 유형이지만, 군집 여부를 따지는 것. 
     // computers 돌면서 연결 요소 갯수 세기 
-    let visited = Array.from({length : n}, () => false);
-    let count = 0;  
+    const visited = Array.from({length: n}, () => false);
+    let count = 0;
     
     function dfs(node){
         visited[node] = true;
-        for(let j = 0; j < n; j++){
-            if(!visited[j] && computers[node][j] === 1){
+       for(let j = 0; j < n; j++){
+            if(!visited[j] && computers[node][j] === 1 ){
                 dfs(j);
             }
         }
+        
     }
     
     for(let i = 0; i < n; i++){
+        // 방문하지 않다면 -> 방문 체크
         if(!visited[i]){
+            dfs(i)
             count++;
-            dfs(i);
         }
-
+        
     }
     
     return count;
+    
 }
